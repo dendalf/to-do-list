a12c4caea08dffb0123033b64481d0fd845e1f05
dendalf <dionisiy.agarkov@gmail.com>
2018-05-13 20:01:06 +0300

    сделал разноцветные li и добавил form

--- to-do-list.js
+++ to-do-list.js
@@ -1,21 +1,58 @@
+// $(document).ready(function(){
+//
+//     function abc() {
+//         let value = $('#input');
+//         let valueInput = value.val();
+//         $('#add-list').append(`<li class='item'>${valueInput}</li>`);
+//         value.value = '';
+//     }
+//
+//     let qwe = $('#button-add');
+//
+//     $(qwe).on( 'click', abc() );
+//
+//     $('#input').keyup(function(){
+//         if(event.keyCode === 13)
+//         {
+//             abc()
+//         }
+//     });
+//
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
 
-    let qwe = $('#button-add');
-    $(qwe).on( 'click', function abc() {
+// if ($('#input').valueInput =! ''){
+
+    $('#button-add').on( 'click',  function () {
         let value = $('#input');
         let valueInput = value.val();
         $('#add-list').append(`<li class='item'>${valueInput}</li>`);
-        value.value = '';
+        $('#input').valueInput = '';
     });
-    $('#input').keyup(function(){
-        if(event.keyCode === 13)
-        {
-            let value = $('#input');
-            let valueInput = value.val();
-            $('#add-list').append(`<li class='item'>${valueInput}</li>`);
-            value.value = '';
-            $('#input').value = '';
-        }
-    })
+
+// }
+    // $('#input').keyup(function(){
+    //     if(event.keyCode === 13)
+    //     {
+    //         let value = $('#input');
+    //         let valueInput = value.val();
+    //         $('#add-list').append(`<li class='item'>${valueInput}</li>`);
+    //         value.value = '';
+    //     }
+    // });
 
 });
