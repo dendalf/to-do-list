e58a6614844c299822357edb7ae2f52c150d1887
dendalf <dionisiy.agarkov@gmail.com>
2018-06-13 20:03:57 +0300

    Очищение input после нажатия кнопки

--- to-do-list.js
+++ to-do-list.js
@@ -1,27 +1,6 @@
-// $(document).ready(function(){
-//
-//     function abc() {
-//         let value = $('#input');
-//         let valueInput = value.val();
-//         $('#add-list').append(`<li class='item'>${valueInput}</li>`);
-//         value.value = '';
-//     }
-//
-//     let qwe = $('#button-add');
-//
-//     $(qwe).on( 'click', abc() );
-//
-//     $('#input').keyup(function(){
-//         if(event.keyCode === 13)
-//         {
-//             abc()
-//         }
-//     });
-//
-// });
-
-
 $(document).ready(function () {
+
+    // Добавляется li
 
     $('#button-add').on('click', function () {
         let value = $('#input');
@@ -29,26 +8,63 @@ $(document).ready(function () {
         $('#add-list').append(`<li class='item'>${valueInput}<div class='remove'>x</div><div class='modal'>m</div></li>`);
     });
 
+    // Удаляется li
+
     $(document).on('click', '.remove', function () {
         $(this).parent().remove()
     });
+
+    // Модальная кнопка
 
     $(document).on('click', '.modal', function () {
         alert('Hello World!')
     });
 
+    // Добавляется зачеркивание
+
     $(document).on('click', '.item', function () {
         $(this).removeClass('item').addClass('checked')
     });
 
-    // $('#input').keyup(function(){
-    //     if(event.keyCode === 13)
-    //     {
-    //         let value = $('#input');
-    //         let valueInput = value.val();
-    //         $('#add-list').append(`<li class='item'>${valueInput}</li>`);
-    //         value.value = '';
+
+
+
+
+
+
+
+
+
+
+    //
+    // function someFunc() {
+    //     let inputVal = document.getElementById("input").value;
+    //     let itemVal = document.getElementsByClassName("item").value;
+    //     if (inputVal !== itemVal) {
+    //         console.log(123)
     //     }
-    // });
+    // }
+    //
+    // document.getElementById("button-add").onclick = someFunc;
+    //
+    //
+
+
+
+
+
+
+
+
+    // Очищается input
+
+    document.getElementById("button-add").onclick = function () {
+        document.getElementById("input").value = "";
+    };
 
 });
+
+
+
+
+
